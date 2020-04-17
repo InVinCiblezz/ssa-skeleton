787b03f4e39486d6080fc01e4d0fb3997c007987
InVinCiblezz <360340028@qq.com>
2020-04-17 02:00:29 -0500

    hw1 finshied basically

--- hw1/src/main/java/edu/northwestern/ssa/App.java
+++ hw1/src/main/java/edu/northwestern/ssa/App.java
@@ -1,7 +1,193 @@
 package edu.northwestern.ssa;
 
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.http.SdkHttpMethod;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+
+import org.archive.io.warc.WARCReaderFactory;
+import org.archive.io.ArchiveRecord;
+import org.archive.io.ArchiveReader;
+import org.apache.commons.io.IOUtils;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.time.Duration;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.Map;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+import java.util.Optional;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Future;
+
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        //Part1
+        String bucketName = "commoncrawl";
+        @// TODO: 2020/4/17 auto-crawl
+        String object_key =  System.getenv("COMMON_CRAWL_FILENAME") != null
+                ? System.getenv("COMMON_CRAWL_FILENAME") : "crawl-data/CC-NEWS/2020/04/CC-NEWS-20200405100433-01526.warc.gz";
+        System.out.println(object_key);
+
+        try{
+            S3Client s3 = S3Client.builder()
+                    .region(Region.US_EAST_1)
+                    .overrideConfiguration(ClientOverrideConfiguration.builder()
+                            .apiCallTimeout(Duration.ofMinutes(30)).build())
+                    .build();
+
+
+            //ListObjectsV2Request listObjectsV2Request =ListObjectsV2Request.builder().bucket(bucketName).maxKeys(2).build();
+
+            //ListObjectsV2Response listObjectsResponse = s3.listObjectsV2(listObjectsV2Request);
+
+            //listObjectsResponse.contents().forEach(content->{
+            //    System.out.println(content.toString());
+            //});
+
+
+            System.out.println("[DOWNLOAD] --- Started ---");
+            s3.getObject(GetObjectRequest.builder().bucket(bucketName).
+                            key(object_key).build(),
+                    ResponseTransformer.toFile(Paths.get("WARC")));
+            System.out.println("[DOWNLOAD] --- Finished ---");
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
+
+        //Part2 & 3 &4
+        System.out.println("[INFO] --- Parse Started ---");
+        try {
+            parseWarc(new File("WARC"));
+        } catch (IOException e) {
+            System.err.print(e);
+        }
+        System.out.println("[INFO] --- Parse Finished ---");
+
+@// TODO: 2020/4/17 multiThread
+/*
+        try{
+            //System.out.println(System.getenv("ELASTIC_SEARCH_HOST"));
+            ElasticSearch es = new ElasticSearch();
+            Map<String, String> map = new HashMap<>();
+
+            //create
+            //es.createIndex(SdkHttpMethod.PUT, Optional.ofNullable(map));
+
+
+            JSONObject body = new JSONObject();
+            body.put("title", "888");
+            body.put("url", "myUrl");
+            body.put("txt", "myTxt");
+            //es.postDoc(SdkHttpMethod.POST, Optional.ofNullable(map), Optional.ofNullable(body));
+
+
+            JSONObject query = new JSONObject(
+                    "{\n" +
+                    "  \"query\": {\n" +
+                    "    \"match\": {\n" +
+                    "      \"txt\": {\n" +
+                    "        \"query\": \"Evanston\"\n" +
+                    "      }\n" +
+                    "    }\n" +
+                    "  }\n" +
+                    "}");
+
+
+            ExecutorService exec = Executors.newSingleThreadExecutor();
+            for (int i = 0; i < 10; i++) {
+                int index = i;
+                Future future = exec.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            es.postDoc(SdkHttpMethod.POST, Optional.ofNullable(map), Optional.ofNullable(body));
+                        } catch (Exception e) {
+                        }
+                        System.out.println(Thread.currentThread() + "----" + index);
+                    }
+                });
+                System.out.println("future.get()=" + future.get());
+            }
+            exec.shutdown();
+
+
+
+
+            // post doc
+            //es.postDoc(SdkHttpMethod.POST, Optional.ofNullable(map), Optional.ofNullable(body));
+            //es.postDoc(SdkHttpMethod.PUT, Optional.ofNullable(map), Optional.ofNullable(body));
+
+            // search doc
+            es.searchDoc(SdkHttpMethod.POST, Optional.ofNullable(map),Optional.ofNullable(query));
+            es.close();
+
+        }catch(Exception ex){
+            System.out.println("[ERROR] --- " + ex);
+        }
+        System.out.println("[INFO] --- Search Finished ---");
+    */
+
+
+
     }
+
+    private static void parseWarc(File f) throws IOException {
+        ArchiveReader r = WARCReaderFactory.get(f);
+        Iterator<ArchiveRecord> it = r.iterator();
+        ArchiveRecord record;
+
+        String s;
+        String[] strs;
+        byte[] bytes;
+        ElasticSearch es = new ElasticSearch();
+        Map<String, String> map = new HashMap<>();
+        es.createIndex(SdkHttpMethod.PUT, Optional.ofNullable(map));
+        JSONObject body;
+        int count = 0;
+        while (it.hasNext()) {
+            try {
+                record = it.next();
+                String url = record.getHeader().getUrl();
+                bytes = IOUtils.toByteArray(record);
+                s = new String(bytes);
+                strs = s.split("\r\n\r\n");
+                if (strs.length == 2) s = strs[1];
+            //System.out.println("[INFO]---s: " + s);
+                Document doc = Jsoup.parse(s);
+                String text = doc.text();
+                String title = doc.title();
+                if (title.length() == 0) continue;
+            //System.out.println("[PARSE] --- Parse ---");
+            //System.out.println("[PARSE] --- URL: " + record.getHeader().getUrl());
+            //System.out.println("[PARSE]--- text: " + text);
+            //System.out.println("[PARSE]--- title: " + title);
+                body = new JSONObject();
+                body.put("title", title);
+                body.put("url", url);
+                body.put("txt", text);
+                es.postDoc(SdkHttpMethod.POST, Optional.ofNullable(map), Optional.ofNullable(body));
+                count++;
+                System.out.println("Count: " + count);
+            } catch (Exception e) {
+                //System.err.print(e);
+            }
+        }
+        es.close();
+    }
+
 }
---
+++ hw1/src/main/java/edu/northwestern/ssa/ElasticSearch.java
@@ -0,0 +1,90 @@
+package edu.northwestern.ssa;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import software.amazon.awssdk.http.AbortableInputStream;
+import software.amazon.awssdk.http.HttpExecuteResponse;
+import software.amazon.awssdk.http.SdkHttpFullResponse;
+import software.amazon.awssdk.http.SdkHttpMethod;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Optional;
+
+public class ElasticSearch extends AwsSignedRestRequest {
+    /**
+     * @param serviceName would be "es" for Elasticsearch
+     */
+    private String indexPath = System.getenv("ELASTIC_SEARCH_INDEX");
+    ElasticSearch() {super("es");}
+
+    public void createIndex(SdkHttpMethod method,  Optional<Map<String, String>> params) {
+        String host = System.getenv("ELASTIC_SEARCH_HOST");
+        try {
+            //printResponse(restRequest(method, host, indexPath, params));
+            restRequest(method, host, System.getenv("ELASTIC_SEARCH_INDEX"), params);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @// TODO: 2020/4/17 bulk
+    public void bulkDoc(SdkHttpMethod method,  Optional<Map<String, String>> params, Optional<JSONObject> body) {
+        String path = System.getenv("ELASTIC_SEARCH_INDEX") + "/_bulk";
+        String host = System.getenv("ELASTIC_SEARCH_HOST");
+        try {
+            //printResponse(restRequest(method, host, path, params, body));
+            HttpExecuteResponse response = restRequest(method, host, path, params, body);
+            printResponse(response);
+            response.responseBody().get().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void postDoc(SdkHttpMethod method,  Optional<Map<String, String>> params, Optional<JSONObject> body) {
+        String path = System.getenv("ELASTIC_SEARCH_INDEX") + "/_doc";
+        String host = System.getenv("ELASTIC_SEARCH_HOST");
+        //System.out.println(body);
+        try {
+            //printResponse(restRequest(method, host, path, params, body));
+            HttpExecuteResponse response = restRequest(method, host, path, params, body);
+            while (!response.httpResponse().isSuccessful()) {
+                response.responseBody().get().close();
+                response = restRequest(method, host, path, params, body);
+            }
+            //String result = printResponse(response);
+            //JSONObject b = new JSONObject(result);
+            //b = (JSONObject)b.get("successful");
+            //System.out.println("b : " + b.toString(4));
+            response.responseBody().get().close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public  void searchDoc(SdkHttpMethod method,  Optional<Map<String, String>> params, Optional<JSONObject> query){
+        String path = indexPath + "/_search";
+        String host = System.getenv("ELASTIC_SEARCH_HOST");
+        System.out.println(host);
+        try {
+            String result = printResponse(restRequest(method, host, path, params, query));
+            JSONObject body = new JSONObject(result);
+            body = (JSONObject)body.get("hits");
+            System.out.println(body.toString(4));
+            close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static String printResponse(HttpExecuteResponse executeResponse) throws IOException {
+        AbortableInputStream inputStream = executeResponse.responseBody().get();
+        StringWriter writer = new StringWriter();
+        String result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        JSONObject body = new JSONObject(result);
+        System.out.println(result);
+        return result;
+    }
+}
